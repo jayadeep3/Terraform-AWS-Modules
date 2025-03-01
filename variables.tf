4d0dda0e5192485bf962e7444e44987625e4b62f
jayadeepallu <109648525+jayadeep3@users.noreply.github.com>
2025-03-01 15:14:06 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,42 +1,10 @@
-variable "region" {
-  type = string
-  default = "ap-south-1"
-}
-
-variable "instance_type" {
-  type    = map(string)
-  default = {
-    dev  = "t2.micro"
-    demo = "t2.small"
-    prod = "t2.medium"
-  }
-}
-
-variable "availability_zone" {
-  type = map(string)
-  default = {
-    dev  = "ap-south-1a"
-    demo = "ap-south-1b"
-  }
-}
-
-variable "subnet_cidr_block" {
-  type = map(string)
-  default = {
-    dev  = "10.0.1.0/24"
-    demo = "10.0.2.0/24"
-  }
-}
-
-variable "vpc_cidr_block" {
-  type = map(string)
-  default = {
-    dev  = "10.0.0.0/16"
-    demo = "10.0.0.0/18"
-  }
-}
-
+variable "aws_region" {}
+variable "vpc_cidr_block" {}
+variable "public_subnet_cidr" {}
+variable "private_subnet_cidr" {}
+variable "availability_zone" {}
+variable "ami_id" {}
 variable "allowed_ssh_cidr" {
-  type = list(string)
-  default = ["0.0.0.0/0"]
+    type = list(string)
 }
+variable "instance_type" {}
