18284c0f924357690791a191a45d955c76d09e70
jayadeepallu <109648525+jayadeep3@users.noreply.github.com>
2025-03-01 16:07:30 +0530

    Add files via upload

---
+++ modules/s3_bucket/variables.tf
@@ -0,0 +1,10 @@
+variable "bucket_name" {
+  description = "The name of the S3 bucket"
+  type        = string
+}
+
+variable "enable_versioning" {
+  description = "Enable versioning for the S3 bucket"
+  type        = bool
+  default     = false
+}
