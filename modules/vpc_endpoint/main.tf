6a59ded091ffb74eb6b444421d530a93a2cb7b4a
jayadeepallu <109648525+jayadeep3@users.noreply.github.com>
2025-03-01 17:49:23 +0530

    Create main.tf

---
+++ modules/vpc_endpoint/main.tf
@@ -0,0 +1,7 @@
+resource "aws_vpc_endpoint" "api_gateway" {
+  vpc_id            = var.vpc_id
+  service_name      = "com.amazonaws.${var.aws_region}.execute-api"
+  vpc_endpoint_type = "Interface"
+  subnet_ids        = var.private_subnet_id.id
+  security_group_ids = var.security_group_id.id
+}
