55c2dbc7a4509531eb1e3863835e759821e1e768
jayadeepallu <109648525+jayadeep3@users.noreply.github.com>
2025-03-04 15:10:32 +0530

    Update main.tf

--- modules/private_ec2/main.tf
+++ modules/private_ec2/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "private" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
-  subnet_id              = var.private_subnet_id.id
+  subnet_id              = var.private_subnet_id[0]
   vpc_security_group_ids = [ var.security_group_id ]
 
   tags = {
