f2d6b0bc9ae1f7231e7a226db898ab14b2ba7499
jayadeepallu <109648525+jayadeep3@users.noreply.github.com>
2025-03-04 15:06:32 +0530

    Update main.tf

--- modules/vpc_endpoint/main.tf
+++ modules/vpc_endpoint/main.tf
@@ -2,6 +2,6 @@ resource "aws_vpc_endpoint" "api_gateway" {
   vpc_id            = var.vpc_id
   service_name      = "com.amazonaws.${var.aws_region}.execute-api"
   vpc_endpoint_type = "Interface"
-  subnet_ids        = var.private_subnet_id.id
-  security_group_ids = var.security_group_id.id
+  subnet_ids        = var.private_subnet_id
+  security_group_ids = [var.security_group_id]
 }
